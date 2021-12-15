b3d037a8eeb2b8b3ea579d974c6ba6264b2fba2d
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-15 22:08:06 +0900

    feat: gip.py: added hash-object to main func

--- gip.py
+++ gip.py
@@ -2,13 +2,15 @@ from argparse import Namespace
 import os
 
 from options import get_options
-from commands import cmd_init
+from commands import cmd_init, cmd_hash_object
 
 
 def main(args: Namespace) -> None:
     if args.command == "init":
         args.directory = os.path.abspath(args.directory)
-    cmd_init(args)
+        cmd_init(args)
+    elif args.command == "hash-object":
+        cmd_hash_object(args)
 
 
 if __name__ == "__main__":
