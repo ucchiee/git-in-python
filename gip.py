b08dcbfd42689367091bc709e30e99f71607b903
Yusuke Uchida <51429010+ucchiee@users.noreply.github.com>
2021-12-15 18:44:11 +0900

    Feat/init (#6)
    * feat: init.py: init command
    
    * chore: commands/__init__.py: initial commit
    
    * feat: get_options.py: added init command option
    
    * fix: options/__init__.py: fixed type
    
    * feat: gip.py: added init command

--- gip.py
+++ gip.py
@@ -1,8 +1,14 @@
+from argparse import Namespace
+import os
+
 from options import get_options
+from commands import cmd_init
 
 
-def main(args):
-    pass
+def main(args: Namespace) -> None:
+    if args.command == "init":
+        args.directory = os.path.abspath(args.directory)
+    cmd_init(args)
 
 
 if __name__ == "__main__":
