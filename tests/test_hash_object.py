95f03881b39ab14c720bfd1c1de9dfb2201736e9
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-21 22:13:20 +0900

    feat: test_hash_object.py: added test for gip hash-object

---
+++ tests/test_hash_object.py
@@ -0,0 +1,15 @@
+import unittest
+
+from commands import cmd_hash_object
+from options import get_parser
+
+
+class TestGipHashObject(unittest.TestCase):
+    def test_hash_ojbect(self):
+        parser = get_parser()
+        args = parser.parse_args(["hash-object", "README.md"])
+        cmd_hash_object(args)
+
+
+if __name__ == "__main__":
+    unittest.main()
