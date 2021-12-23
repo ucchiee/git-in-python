38417869e1b4c28e42a09f545e14f1c968e3777f
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-23 16:37:24 +0900

    gip.py,init.py: changed interface of init command

--- gip.py
+++ gip.py
@@ -1,5 +1,4 @@
 from argparse import Namespace
-import os
 
 from commands import cmd_init, cmd_hash_object
 from options import get_options
@@ -7,7 +6,6 @@ from options import get_options
 
 def main(args: Namespace) -> None:
     if args.command == "init":
-        args.directory = os.path.abspath(args.directory)
         cmd_init(args)
     elif args.command == "hash-object":
         cmd_hash_object(args)
