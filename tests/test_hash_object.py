cb560eb6da28804d785085a863044d31093f9cde
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-23 15:46:16 +0900

    test_hash_object.py: added setup and teardown

--- tests/test_hash_object.py
+++ tests/test_hash_object.py
@@ -1,15 +1,25 @@
 import unittest
+import os
+import shutil
 
 # from commands import cmd_hash_object
 from options import get_parser
 
+dirname: str = os.path.abspath("./tmp_dir")
+
 
 class TestGipHashObject(unittest.TestCase):
+    def setUp(self) -> None:
+        os.makedirs(dirname, exist_ok=True)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(dirname)
+
     def test_hash_object(self):
-        # parser = get_parser()
-        # args = parser.parse_args(["hash-object", "README.md"])
+        parser = get_parser()
+        args = parser.parse_args(["hash-object", "README.md"])
+        self.assertNotEqual(args, None)
         # cmd_hash_object(args)
-        return
 
 
 if __name__ == "__main__":
