1962960b2e06f5f3f0f79aa99092f4c07206d087
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-24 22:36:41 +0900

    commands/__init__.py: added cmd_cat_file

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,7 +1,9 @@
 from commands.init import cmd_init
 from commands.hash_object import cmd_hash_object
+from commands.cat_file import cmd_cat_file
 
 __all__ = [
     "cmd_init",
     "cmd_hash_object",
+    "cmd_cat_file",
 ]
