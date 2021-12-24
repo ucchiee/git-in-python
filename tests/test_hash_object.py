50374453f4857d3bc74ef0d45c0796e72b6a7929
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-24 11:54:12 +0900

    test_hash_object.py: changed to text binary and multibytes text

--- tests/test_hash_object.py
+++ tests/test_hash_object.py
@@ -1,11 +1,13 @@
 import os
 import shutil
 import unittest
+import subprocess
 
 from commands import cmd_hash_object, cmd_init
 from options import get_parser
 
-dirname: str = os.path.abspath("./tmp_dir_hash")
+tests_dir = os.path.abspath(os.path.dirname(__file__))
+dirname: str = os.path.join(tests_dir, "test_repo")
 
 
 class TestGipHashObject(unittest.TestCase):
@@ -16,21 +18,33 @@ class TestGipHashObject(unittest.TestCase):
         args = parser.parse_args(["init", dirname])
         cmd_init(args)
 
-        # add test file
-        test_path = os.path.join(dirname, "README.md")
-        with open(test_path, mode="w") as f:
-            f.write("# test\n\nThis is a test file.")
-
     def tearDown(self) -> None:
-        shutil.rmtree(dirname)
+        dot_git_dir = os.path.join(dirname, ".git")
+        shutil.rmtree(dot_git_dir)
 
     def test_hash_object_text(self):
         os.chdir(dirname)
         parser = get_parser()
-        args = parser.parse_args(["hash-object", "README.md"])
-        self.assertNotEqual(args, None)
+        args = parser.parse_args(["hash-object", "test.c"])
         sha1_hash = cmd_hash_object(args)
-        self.assertEqual(sha1_hash, "1ac21c5f405ba481cf68620ac7802d3ac62adeef")
+        proc = subprocess.run("git hash-object test.c", shell=True, text=True, stdout=subprocess.PIPE)
+        self.assertEqual(sha1_hash, proc.stdout.strip())
+
+    def test_hash_object_multibytes_text(self):
+        os.chdir(dirname)
+        parser = get_parser()
+        args = parser.parse_args(["hash-object", "japanese.txt"])
+        sha1_hash = cmd_hash_object(args)
+        proc = subprocess.run("git hash-object japanese.txt", shell=True, text=True, stdout=subprocess.PIPE)
+        self.assertEqual(sha1_hash, proc.stdout.strip())
+
+    def test_hash_object_binary(self):
+        os.chdir(dirname)
+        parser = get_parser()
+        args = parser.parse_args(["hash-object", "a.out"])
+        sha1_hash = cmd_hash_object(args)
+        proc = subprocess.run("git hash-object a.out", shell=True, text=True, stdout=subprocess.PIPE)
+        self.assertEqual(sha1_hash, proc.stdout.strip())
 
 
 if __name__ == "__main__":
