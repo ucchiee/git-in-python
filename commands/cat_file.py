ca623c9540a60ac0036365a7c9b1c94930d9ac64
Yusuke Uchida <fantomyuu0623@gmail.com>
2021-12-27 16:49:38 +0900

    cat_file.py: changed to use util

--- commands/cat_file.py
+++ commands/cat_file.py
@@ -1,34 +1,12 @@
-import os
-import glob
 from argparse import Namespace
 
-from util import detect_dot_git
+from util import cat_file
 
 
 def cmd_cat_file(args: Namespace) -> str:
-    # can't specify obj
-    if len(args.hash_value) < 4:
-        print(f"fatal: Not a valid object name {args.hash_value}")
-        return ""
-
-    dot_git_dir: str = detect_dot_git(os.getcwd())
-    obj_dir: str = os.path.join(dot_git_dir, "objects")
-
-    # object is stored in f".git/objects/{hash_value[:2]}/{hash_value[2:]}"
-    # hash_value doesn't need to be complete as long as we can specify unique obj
-    obj_path = os.path.join(obj_dir, args.hash_value[:2], f"{args.hash_value[2:]}*")
-    obj_path_list = glob.glob(obj_path)
-    if len(obj_path_list) == 0:
-        print(f"there is no object that starts with {args.hash_value}")
-        return ""
-    elif len(obj_path_list) > 1:
-        print(f"can't specify unique object by {args.hash_value}")
-        return ""
-
-    with open(obj_path_list[0], mode="r") as f:
-        contents = f.read()
-    print(contents)
-    return contents
+    result = cat_file(args.hash_value, print_error=True)
+    print(result)
+    return result
 
 
 def main():
