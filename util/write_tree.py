430e15e04b37ea760f7ef2b92ebbde6b1a2b6c6f
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-04 19:44:21 +0900

    fix: write_tree.py: fixed null

--- util/write_tree.py
+++ util/write_tree.py
@@ -8,6 +8,6 @@ def write_tree(path_in_repo) -> str:
     t_entry: bytes = b""
     for i_entry in read_index(path_in_repo):
         t_entry += f"{i_entry.mode:o} {i_entry.path}".encode()
-        t_entry += b"0x00" + i_entry.sha1
+        t_entry += b"\x00" + i_entry.sha1
     sha1_hash = write_object("tree", t_entry, os.getcwd())
     return sha1_hash
