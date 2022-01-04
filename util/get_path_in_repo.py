c7cfac9dc537b731e02369c8f05ac9779e255e52
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-04 15:16:08 +0900

    feat: util/get_path_in_repo: added an util to get path relative from repo root

---
+++ util/get_path_in_repo.py
@@ -0,0 +1,9 @@
+import os
+from util.detect_dot_git import detect_dot_git
+
+
+def get_path_in_repo(path: str) -> str:
+    dot_git_dir = detect_dot_git(path)
+    repo_root = os.path.dirname(dot_git_dir)
+    path = os.path.abspath(path)
+    return path.replace(repo_root, "")[1:]
