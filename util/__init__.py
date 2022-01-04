3c0d872dfdb3f8f2a2a3687c3e2cc56795842991
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-04 21:27:17 +0900

    feat: heads.py: implemented head-related util

--- util/__init__.py
+++ util/__init__.py
@@ -1,8 +1,8 @@
 from util.cat_file import cat_file
 from util.detect_dot_git import detect_dot_git
-from util.heads import get_current_branch, get_head_hash
 from util.get_path_in_repo import get_path_in_repo
 from util.hash_object import hash_object
+from util.heads import get_current_branch, read_head_hash, write_head_hash
 from util.objects import read_object, write_object
 from util.read_index import read_index
 from util.write_index import write_index
@@ -12,11 +12,12 @@ __all__ = [
     "cat_file",
     "detect_dot_git",
     "get_current_branch",
-    "get_head_hash",
     "get_path_in_repo",
     "hash_object",
+    "read_head_hash",
     "read_index",
     "read_object",
+    "write_head_hash",
     "write_index",
     "write_object",
     "write_tree",
--- util/heads.py
+++ util/heads.py
@@ -2,35 +2,64 @@ import os
 from util.detect_dot_git import detect_dot_git
 
 
-def read_head(path_in_repo: str) -> str:
+def _read_head(path_in_repo: str) -> str:
     dot_git_dir = detect_dot_git(path_in_repo)
     head_path = os.path.join(dot_git_dir, "HEAD")
+    if not os.path.exists(head_path):
+        return ""
     with open(head_path, mode="r") as f:
         head_content = f.read().strip()
     return head_content
 
 
-def get_head_hash(path_in_repo: str) -> str:
+def _write_head(path_in_repo: str, contents: str) -> None:
     dot_git_dir = detect_dot_git(path_in_repo)
-    head_content = read_head(dot_git_dir)
+    head_path = os.path.join(dot_git_dir, "HEAD")
+    with open(head_path, mode="w") as f:
+        f.write(contents)
 
-    commit_hash: str
-    if head_content.startswith("ref: "):
-        refs_path = head_content.replace("ref: ", "").strip()
-        refs_path = os.path.join(dot_git_dir, refs_path)
-        with open(refs_path, mode="r") as f:
-            commit_hash = f.read().strip()
-    else:
-        commit_hash = head_content.strip()
-    return commit_hash
+
+def _read_branch(path_in_repo: str, branch: str) -> str:
+    dot_git_dir = detect_dot_git(path_in_repo)
+    ref_path = os.path.join(dot_git_dir, "refs", "heads", branch)
+    if not os.path.exists(ref_path):
+        return ""
+    with open(ref_path, mode="r") as f:
+        branch_content = f.read().strip()
+    return branch_content
+
+
+def _write_branch(path_in_repo: str, branch: str, contents: str) -> None:
+    dot_git_dir = detect_dot_git(path_in_repo)
+    ref_path = os.path.join(dot_git_dir, "refs", "heads", branch)
+    with open(ref_path, mode="w") as f:
+        f.write(contents)
 
 
 def get_current_branch(path_in_repo: str) -> str:
     dot_git_dir = detect_dot_git(path_in_repo)
-    head_content = read_head(dot_git_dir)
+    head_content = _read_head(dot_git_dir)
     branch: str
     if head_content.startswith("ref: "):
         branch = head_content.replace("ref: refs/heads/", "")
     else:
         branch = ""
     return branch
+
+
+def read_head_hash(path_in_repo: str) -> str:
+    dot_git_dir = detect_dot_git(path_in_repo)
+    branch = get_current_branch(path_in_repo)
+    if branch:
+        return _read_branch(dot_git_dir, branch)
+    else:
+        return _read_head(dot_git_dir)
+
+
+def write_head_hash(path_in_repo: str, hash: str) -> None:
+    dot_git_dir = detect_dot_git(path_in_repo)
+    branch = get_current_branch(dot_git_dir)
+    if branch:
+        _write_branch(dot_git_dir, branch, hash)
+    else:
+        _write_head(dot_git_dir, hash)
