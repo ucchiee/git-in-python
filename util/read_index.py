b0793b5f80545036970c079f29037428bfd87d67
Yusuke Uchida <fantomyuu0623@gmail.com>
2022-01-05 12:24:11 +0900

    refactor: read_index.py: changed to use assert when possible

--- util/read_index.py
+++ util/read_index.py
@@ -8,34 +8,23 @@ from util.detect_dot_git import detect_dot_git
 
 def read_index(path_in_repo: str, print_error: bool = False) -> list[IndexEntry]:
     dot_git_dir: str = detect_dot_git(path_in_repo)
-    if not dot_git_dir:
-        if print_error:
-            print("Not in a git repository")
-        return []
+    assert dot_git_dir, "Not in a git repository"
 
     index_path: str = os.path.join(dot_git_dir, "index")
     if not os.path.exists(index_path):
-        if print_error:
-            print("there is not a index file yet")
         return []
 
     with open(index_path, mode="rb") as f:
         index_data: bytes = f.read()
 
     checksum: bytes = sha1(index_data[:-20]).digest()
-    if checksum != index_data[-20:]:
-        if print_error:
-            print("invalid index")
-        return []
+    assert checksum == index_data[-20:], "invalid index"
 
     signature, version, num_entries = struct.unpack("!4sLL", index_data[:12])
-    if signature != b"DIRC":
-        if print_error:
-            print("invalid signature, must be DIRC")
-        return []
+    assert signature == b"DIRC", "invalid signature, must be DIRC"
     if version != 2:
         if print_error:
-            print("invalid version, must be 2")
+            print(f"do not support {version}, only support 2")
         return []
 
     index_data = index_data[12:-20]
